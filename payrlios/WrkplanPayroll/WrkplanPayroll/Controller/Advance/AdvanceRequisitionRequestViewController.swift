497c300673815a59a2ef12ab3389f68499b90845
Satabhisha Roy <satabhisha.innits@gmail.com>
2021-06-18 14:05:05 +0530

    modified

--- payrlios/WrkplanPayroll/WrkplanPayroll/Controller/Advance/AdvanceRequisitionRequestViewController.swift
+++ payrlios/WrkplanPayroll/WrkplanPayroll/Controller/Advance/AdvanceRequisitionRequestViewController.swift
@@ -37,7 +37,7 @@ class AdvanceRequisitionRequestViewController: UIViewController {
         
         ChangeStatusBarColor() //---to change background statusbar color
         
-        
+       
         //---code to customize dropdown button starts------
         let buttonWidth = btn_reason_select_type.frame.width
         let imageWidth = btn_reason_select_type.imageView!.frame.width
@@ -240,4 +240,29 @@ class AdvanceRequisitionRequestViewController: UIViewController {
     }
     //----function to load buttons acc to the logic, code ends
     
+   
+    
+};
+
+//---code to set max limit option inside stoyboard at textfield section, starts-----
+private var __maxLengths = [UITextField: Int]()
+extension UITextField {
+    @IBInspectable var maxLength: Int {
+        get {
+            guard let l = __maxLengths[self] else {
+                return 150 // (global default-limit. or just, Int.max)
+            }
+            return l
+        }
+        set {
+            __maxLengths[self] = newValue
+            addTarget(self, action: #selector(fix), for: .editingChanged)
+        }
+    }
+    @objc func fix(textField: UITextField) {
+        if let t = textField.text {
+            textField.text = String(t.prefix(maxLength))
+        }
+    }
 }
+//---code to set max limit option inside stoyboard at textfield section, ends-----
