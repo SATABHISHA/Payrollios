dba31c7cf90e1258a935546bda7f052ef90abe85
Satabhisha Roy <satabhisha.innits@gmail.com>
2021-06-28 19:49:54 +0530

    modified

---
+++ payrlios/WrkplanPayroll/WrkplanPayroll/Controller/Mediclaim/MediclaimListViewController.swift
@@ -0,0 +1,29 @@
+//
+//  MediclaimListViewController.swift
+//  WrkplanPayroll
+//
+//  Created by SATABHISHA ROY on 28/06/21.
+//
+
+import UIKit
+
+class MediclaimListViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
