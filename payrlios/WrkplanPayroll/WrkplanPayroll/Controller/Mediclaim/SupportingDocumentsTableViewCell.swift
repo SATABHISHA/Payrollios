1086454a32afc6b9f5c4c957662080f7684c04b7
Satabhisha Roy <satabhisha.innits@gmail.com>
2021-06-30 18:14:11 +0530

    modified

---
+++ payrlios/WrkplanPayroll/WrkplanPayroll/Controller/Mediclaim/SupportingDocumentsTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  SupportingDocumentsTableViewCell.swift
+//  WrkplanPayroll
+//
+//  Created by SATABHISHA ROY on 30/06/21.
+//
+
+import UIKit
+
+class SupportingDocumentsTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
