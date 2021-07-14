4f5e7ea6afc48eb31cdc803854a0a3a7120b09b0
Satabhisha Roy <satabhisha.innits@gmail.com>
2021-07-14 14:19:51 +0530

    modified

---
+++ payrlios/WrkplanPayroll/WrkplanPayroll/Controller/Lta/LtaListTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  LtaListTableViewCell.swift
+//  WrkplanPayroll
+//
+//  Created by SATABHISHA ROY on 14/07/21.
+//
+
+import UIKit
+
+class LtaListTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
