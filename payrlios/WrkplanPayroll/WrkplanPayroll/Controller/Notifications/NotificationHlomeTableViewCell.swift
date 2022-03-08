dea06a4f87b90b31dc2c4ab2eb15ee8578e76d8f
Satabhisha Roy <satabhisha.innits@gmail.com>
2022-03-08 12:13:04 +0530

    modified

---
+++ payrlios/WrkplanPayroll/WrkplanPayroll/Controller/Notifications/NotificationHlomeTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  NotificationHlomeTableViewCell.swift
+//  WrkplanPayroll
+//
+//  Created by Debashis Pal on 08/03/22.
+//
+
+import UIKit
+
+class NotificationHlomeTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
---
+++ payrlios/WrkplanPayroll/WrkplanPayroll/Controller/Notifications/NotificationHomeViewController.swift
@@ -0,0 +1,22 @@
+//
+//  NotificationHomeViewController.swift
+//  WrkplanPayroll
+//
+//  Created by Debashis Pal on 08/03/22.
+//
+
+import UIKit
+
+class NotificationHomeViewController: UIViewController {
+
+    @IBOutlet weak var NotificationHomeTableView: UITableView!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    
+
+}
