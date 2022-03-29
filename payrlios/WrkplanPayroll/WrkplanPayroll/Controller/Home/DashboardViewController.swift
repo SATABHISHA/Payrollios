108258be2209cf201b129b5b959d7dfd2f27dc20
Satabhisha Roy <satabhisha.innits@gmail.com>
2022-03-29 17:35:51 +0530

    modified

--- payrlios/WrkplanPayroll/WrkplanPayroll/Controller/Home/DashboardViewController.swift
+++ payrlios/WrkplanPayroll/WrkplanPayroll/Controller/Home/DashboardViewController.swift
@@ -67,6 +67,8 @@ class DashboardViewController: UIViewController, CLLocationManagerDelegate, UIIm
     var arrResCaledar = [[String:AnyObject]]()
     @IBOutlet weak var LabelCalendarDate: UILabel!
     @IBOutlet weak var LabelCalendarDay: UILabel!
+    @IBOutlet weak var ViewCustomBtnApplyForLeave: UIView!
+    @IBOutlet weak var ViewCustomBtnApplyForOD: UIView!
     //-------Calendar variable, ends----
     
     
@@ -644,6 +646,9 @@ class DashboardViewController: UIViewController, CLLocationManagerDelegate, UIIm
         calendar.clipsToBounds = true
         calendar.layer.cornerRadius = 10
         calendar.layer.maskedCorners = [.layerMinXMinYCorner, .layerMaxXMinYCorner]
+        
+        calendar.addBorderFSCCalendar(side: .bottom, color: UIColor(hexFromString: "BFBFBF"), width: 1)
+        
 //        calender.appearance.separators = .interRows
     }
     //===========Calender code ends============
@@ -955,6 +960,40 @@ extension DashboardViewController: FSCalendarDataSource, FSCalendarDelegate, FSC
     }
     
 };
+
+extension FSCalendar {
+
+    public enum BorderSide {
+        case top, bottom, left, right
+    }
+
+    public func addBorderFSCCalendar(side: BorderSide, color: UIColor, width: CGFloat) {
+            let border = UIView()
+            border.translatesAutoresizingMaskIntoConstraints = false
+            border.backgroundColor = color
+            self.addSubview(border)
+
+            let topConstraint = topAnchor.constraint(equalTo: border.topAnchor)
+            let rightConstraint = trailingAnchor.constraint(equalTo: border.trailingAnchor)
+            let bottomConstraint = bottomAnchor.constraint(equalTo: border.bottomAnchor)
+            let leftConstraint = leadingAnchor.constraint(equalTo: border.leadingAnchor)
+            let heightConstraint = border.heightAnchor.constraint(equalToConstant: width)
+            let widthConstraint = border.widthAnchor.constraint(equalToConstant: width)
+
+
+            switch side {
+            case .top:
+                NSLayoutConstraint.activate([leftConstraint, topConstraint, rightConstraint, heightConstraint])
+            case .right:
+                NSLayoutConstraint.activate([topConstraint, rightConstraint, bottomConstraint, widthConstraint])
+            case .bottom:
+                NSLayoutConstraint.activate([rightConstraint, bottomConstraint, leftConstraint, heightConstraint])
+            case .left:
+                NSLayoutConstraint.activate([bottomConstraint, leftConstraint, topConstraint, widthConstraint])
+            }
+        }
+}
+
 /*extension UIView {
 
     func roundCorners(_ corners: UIRectCorner, radius: CGFloat) {
