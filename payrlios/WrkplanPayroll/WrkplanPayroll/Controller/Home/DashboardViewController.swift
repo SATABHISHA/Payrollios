10fc479c1225aadc2578f021f917a7ab2bda0a17
Satabhisha Roy <satabhisha.innits@gmail.com>
2022-03-31 16:51:27 +0530

    modified

--- payrlios/WrkplanPayroll/WrkplanPayroll/Controller/Home/DashboardViewController.swift
+++ payrlios/WrkplanPayroll/WrkplanPayroll/Controller/Home/DashboardViewController.swift
@@ -12,12 +12,14 @@ import CoreLocation
 import Toast_Swift
 import FSCalendar
 import DropDown
+import UserNotifications
+import CoreData
 
 struct NavigationDashboardMenuData{
     var imageData:UIImage!
     var menuItm:String!
 }
-class DashboardViewController: UIViewController, CLLocationManagerDelegate, UIImagePickerControllerDelegate, UINavigationControllerDelegate, UITableViewDataSource, UITableViewDelegate {
+class DashboardViewController: UIViewController, CLLocationManagerDelegate, UIImagePickerControllerDelegate, UINavigationControllerDelegate, UITableViewDataSource, UITableViewDelegate, UNUserNotificationCenterDelegate {
 
     @IBOutlet weak var ScrollViewContainer: UIView!
     @IBOutlet weak var ScrollView: UIScrollView!
@@ -27,6 +29,16 @@ class DashboardViewController: UIViewController, CLLocationManagerDelegate, UIIm
     let sharedpreferences = UserDefaults.standard
     //----Menubar variables, ends-----
     
+    //-----Notification variable, starts-----
+    let userNotificationCenter = UNUserNotificationCenter.current() 
+    let authOptions = UNAuthorizationOptions.init(arrayLiteral: .alert, .badge, .sound)
+    
+    var db: OpaquePointer? //---added on 09-Mar-2022
+    var arrResNotification = [[String:Any]]()
+    
+    static var NotificationYN: Bool = false
+    @IBOutlet weak var NotificationImageView: UIImageView!
+    //-----Notification variable, ends-----
     
     //---------variables for navigation drawer starts-------
     @IBOutlet weak var navigationDesignation: UILabel!
@@ -133,6 +145,7 @@ class DashboardViewController: UIViewController, CLLocationManagerDelegate, UIIm
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+        LoadNotificationDetails()
         LoadNavigationBarData()
         LoadNavigationDrawerData()
         ChangeStatusBarColor()
@@ -143,6 +156,321 @@ class DashboardViewController: UIViewController, CLLocationManagerDelegate, UIIm
         LoadSalaryData()
         
     }
+    //-------/////---Notification, code starts--////-----
+    func LoadNotificationDetails(){
+        NotificationImageView.isHidden = true
+        if fetchNotificationCount(employeeId: swiftyJsonvar1["employee"]["employee_id"].stringValue) == true {
+            NotificationImageView.isHidden = false
+        }else if fetchNotificationCount(employeeId: swiftyJsonvar1["employee"]["employee_id"].stringValue) == false{
+            NotificationImageView.isHidden = true
+        }
+        
+        self.userNotificationCenter.delegate = self
+        
+        self.requestNotificationAuthorization()
+        
+        Timer.scheduledTimer(withTimeInterval: 10.0, repeats: true) { (timer) in
+                // Do what you need to do repeatedly
+            self.LoadNotificationData()
+            }
+        
+        //NotificationView
+        let tapGestureRecognizerImgNotification = UITapGestureRecognizer(target: self, action: #selector(ImgNotification(tapGestureRecognizer:)))
+        NotificationImageView.isUserInteractionEnabled = true
+        NotificationImageView.addGestureRecognizer(tapGestureRecognizerImgNotification)
+    }
+    //---Lta
+    @objc func ImgNotification(tapGestureRecognizer: UITapGestureRecognizer){
+        self.performSegue(withIdentifier: "notification", sender: nil)
+    }  
+    func userNotificationCenter(_ center: UNUserNotificationCenter, didReceive response: UNNotificationResponse, withCompletionHandler completionHandler: @escaping () -> Void) {
+        completionHandler()
+    }
+    
+    func userNotificationCenter(_ center: UNUserNotificationCenter, willPresent notification: UNNotification, withCompletionHandler completionHandler: @escaping (UNNotificationPresentationOptions) -> Void) {
+        completionHandler([.alert, .badge, .sound])
+    }
+    
+    func requestNotificationAuthorization() {
+        let authOptions = UNAuthorizationOptions.init(arrayLiteral: .alert, .badge, .sound)
+        
+        self.userNotificationCenter.requestAuthorization(options: authOptions) { (success, error) in
+            if let error = error {
+                print("Error: ", error)
+            }
+        }
+    }
+    
+    func sendNotification(title: String, body: String) {
+        // Create new notifcation content instance
+        let notificationContent = UNMutableNotificationContent()
+        
+        // Add the content to the notification content
+        notificationContent.title = title
+        notificationContent.body = body
+        notificationContent.badge = NSNumber(value: 3)
+        
+        // Add an attachment to the notification content
+        if let url = Bundle.main.url(forResource: "done",
+                                     withExtension: "png") {
+            if let attachment = try? UNNotificationAttachment(identifier: "done",
+                                                              url: url,
+                                                              options: nil) {
+                notificationContent.attachments = [attachment]
+            }
+        }
+        let trigger = UNTimeIntervalNotificationTrigger(timeInterval: 5,
+                                                        repeats: false)
+        let request = UNNotificationRequest(identifier: "testNotification",
+                                            content: notificationContent,
+                                            trigger: trigger)
+        userNotificationCenter.add(request) { (error) in
+            if let error = error {
+                print("Notification Error: ", error)
+            }
+        }
+    }
+    
+    func fetchNotificationCount(employeeId: String)-> Bool{
+        let appDelegate = UIApplication.shared.delegate as! AppDelegate
+        let context = appDelegate.persistentContainer.viewContext
+        
+        let fetchRequest = NSFetchRequest<NSFetchRequestResult>(entityName: "UserNotification")
+//            fetchRequest.predicate = NSPredicate(format: "companyOffice == %@ AND employNo == %@", companyOffice, employNo)
+        fetchRequest.predicate = NSPredicate(format: "employeeid == %@", employeeId)
+
+            let res = try! context.fetch(fetchRequest)
+        print("countHome-=>", res)
+            return !res.isEmpty
+    }
+    
+    func CustomNotificationUpdate(notificationId: Int){
+        let url = "\(BASE_URL)notification/custom/update"
+        
+        let sentData: [String: Any] = [
+            "corp_id": swiftyJsonvar1["company"]["corporate_id"].stringValue,
+            "notification_id": notificationId
+        ]
+        
+        print("SentData-=>",sentData)
+        
+        AF.request(url, method: .post, parameters: sentData, encoding: JSONEncoding.default, headers: nil).responseJSON{
+            response in
+            switch response.result{
+                
+            case .success:
+                //                        self.loaderEnd()
+                let swiftyJsonVar = JSON(response.value!)
+                print("Return notifictaion response: ", swiftyJsonVar)
+                
+                
+                break
+                
+            case .failure(let error):
+                //                        self.loaderEnd()
+                print("Error: ", error)
+            }
+        }
+        
+    }
+    func LoadNotificationData(){
+        let url = "\(BASE_URL)notification/custom/fetch/\(swiftyJsonvar1["company"]["corporate_id"].stringValue)/\(swiftyJsonvar1["employee"]["employee_id"].stringValue)/"
+        print("NotificationUrl-=>",url)
+        AF.request(url).responseJSON{ (responseData) -> Void in
+            //               self.loaderEnd()
+            if((responseData.value) != nil){
+                let swiftyJsonVar=JSON(responseData.value!)
+                print("Log Notification description: \(swiftyJsonVar)")
+                
+                if !self.arrResNotification.isEmpty{
+                    self.arrResNotification.removeAll()
+                }
+                
+                if let resData = swiftyJsonVar["notifications"].arrayObject{
+                    self.arrResNotification = resData as! [[String:AnyObject]]
+                }
+                if swiftyJsonVar["response"]["status"] == "true"{
+                    print("Eureka")
+                    if self.arrResNotification.count > 0 {
+                        //Storing core data
+                        //----code to insert data, starts---
+//                        self.resetAllRecords(in: "UserNotification") //---commented on 11-Mar-2022
+                        let appDelegate = UIApplication.shared.delegate as! AppDelegate
+                        let context = appDelegate.persistentContainer.viewContext
+                        let UserNotification = NSEntityDescription.insertNewObject(forEntityName: "UserNotification", into: context)
+                        for items in self.arrResNotification{
+                            UserNotification.setValue("\(self.swiftyJsonvar1["employee"]["employee_id"].stringValue)", forKey: "employeeid")
+                            UserNotification.setValue("\(items)", forKey: "jsondata")
+                            
+                            var title: String = items["title"] as! String
+                            
+                            let body : String = items["body"] as! String
+                            let fullbodyNotificationArr : [String] = body.components(separatedBy: "::")
+                            var notificationId : String = fullbodyNotificationArr[0]
+                            let fullbodyNotificationIdArray: [String] = notificationId.components(separatedBy: "=")
+                            var notification_id_output : String = fullbodyNotificationIdArray[1]
+                            
+                            var event_name : String = fullbodyNotificationArr[1]
+                            let fullbodyEventNameArray: [String] = event_name.components(separatedBy: "=")
+                            var event_name_output : String = fullbodyEventNameArray[1]
+                            
+                            var event_id : String = fullbodyNotificationArr[2]
+                            let fullbodyEventIdArray: [String] = event_id.components(separatedBy: "=")
+                            var event_id_output : String = fullbodyEventIdArray[1]
+                            
+                            var event_owner : String = fullbodyNotificationArr[3]
+                            let fullbodyEventOwnerArray: [String] = event_owner.components(separatedBy: "=")
+                            var event_owner_output : String = fullbodyEventOwnerArray[1]
+                            
+                            var event_owner_id : String = fullbodyNotificationArr[4]
+                            let fullbodyEventOwnerIdArray: [String] = event_owner_id.components(separatedBy: "=")
+                            var event_owner_id_output : String = fullbodyEventOwnerIdArray[1]
+                            
+                            var message : String = fullbodyNotificationArr[5]
+                            let fullbodyMessageArray: [String] = message.components(separatedBy: "=")
+                            var message_output : String = fullbodyMessageArray[1]
+                            
+                            UserNotification.setValue("\(event_id_output)", forKey: "event_id")
+                            UserNotification.setValue("\(event_name_output)", forKey: "event_name")
+                            UserNotification.setValue("\(event_owner_output)", forKey: "event_owner")
+                            UserNotification.setValue("\(event_owner_id_output)", forKey: "event_owner_id")
+                            UserNotification.setValue("\(message_output)", forKey: "message")
+                            UserNotification.setValue("\(title)", forKey: "title")
+                            UserNotification.setValue("\(notification_id_output)", forKey: "notificationid")
+                            UserNotification.setValue("N", forKey: "readyn")
+                            do{
+                               try context.save()
+                                print("Saved")
+                            }catch{
+                                print("Error in saving data")
+                            }
+                        }
+                       
+                            
+                            //---code to fetch data, starts
+                            let request = NSFetchRequest<NSFetchRequestResult>(entityName: "UserNotification")
+                            request.returnsObjectsAsFaults = false
+                            do{
+                                let results = try context.fetch(request)
+                                if results.count > 0
+                                {
+                                    for result in results as! [NSManagedObject]
+                                    {
+                                        if let jsonData = result.value(forKey: "jsondata") as? String{
+                                            print("jsonNotificationData-=>", jsonData)
+                                            
+                                        }
+                                       
+                                        print("All results: ",result)
+                                    }
+                                          if let items = swiftyJsonVar["notifications"].array {
+                                              for item in items {
+                                                 /* if let title = item["title"].string {
+                                                      print(title)
+                                                  }*/
+                                                  var title: String = item["title"].stringValue
+                                                  let body : String = item["body"].stringValue
+                                                  let fullbodyArr : [String] = body.components(separatedBy: "::")
+                                                  
+                                                  var message : String = fullbodyArr[5]
+                                                  var fullMessageArr: [String] = message.components(separatedBy: "=")
+                                                  var messageOutput : String = fullMessageArr[1]
+                                                  
+                                                  var event_id: String = fullbodyArr[0]
+                                                  let fullbodyEventIdArray: [String] = event_id.components(separatedBy: "=")
+                                                  var event_id_output : String = fullbodyEventIdArray[1]
+                                                  print("Message-=>", messageOutput)
+                                                  print("EventId-=>", event_id_output)
+                                                  
+                                                  self.sendNotification(title: title, body: messageOutput)
+                                                  self.CustomNotificationUpdate(notificationId: Int(event_id_output)!)
+                                              }
+                                          }
+                                    
+                                    self.update(readyn: "N")
+                                     }
+                                   
+//                            self.update(readyn: "N")
+                            
+                            
+                        }catch{
+                            //PROCESS ERROR
+                        }
+                        //----code to insert data, ends---
+                    }
+                }else if swiftyJsonVar["status"] == "false"{
+                    print("false")
+                }
+                
+            }
+            
+        }
+    }
+    func resetAllRecords(in entity : String) // entity = Your_Entity_Name
+    {
+        
+        let context = ( UIApplication.shared.delegate as! AppDelegate ).persistentContainer.viewContext
+        let deleteFetch = NSFetchRequest<NSFetchRequestResult>(entityName: entity)
+        let deleteRequest = NSBatchDeleteRequest(fetchRequest: deleteFetch)
+        do
+        {
+            try context.execute(deleteRequest)
+            try context.save()
+            print("Database cleaned")
+        }
+        catch
+        {
+            print ("There was an error")
+        }
+    }
+    func update(readyn:String){
+        
+        //1
+        guard let appDelegate =
+                UIApplication.shared.delegate as? AppDelegate else {
+                    return
+                }
+        
+        let managedContext =
+        appDelegate.persistentContainer.viewContext
+        
+        //2
+        let fetchRequest =
+        NSFetchRequest<NSManagedObject>(entityName: "UserNotification")
+        
+        // 3
+        let predicate = NSPredicate(format: "%K == %@", "readyn", readyn)
+        fetchRequest.predicate = predicate
+        
+        //3
+        
+        do {
+            let  rs = try managedContext.fetch(fetchRequest)
+            
+            for result in rs as [NSManagedObject] {
+                
+                // update
+                do {
+                    var managedObject = rs[0]
+                    managedObject.setValue("Y", forKey: "readyn")
+                    
+                    try managedContext.save()
+                    print("update successfull")
+                    
+                    
+                } catch let error as NSError {
+                    print("Could not Update. \(error), \(error.userInfo)")
+                }
+                //end update
+                
+            }
+            
+        } catch let error as NSError {
+            print("Could not fetch. \(error), \(error.userInfo)")
+        }
+    }
+    //-------/////---Notification, code ends--////-----
+    
     
     //-------///////--Navigation Drawer, code starts--///////-----
     func LoadNavigationDrawerData(){
